102ae288dc0df4186023e813cbffd4af669057ba
Siddhant Mittal <siddhantmittal@Siddhants-MacBook-Pro.local>
2018-09-15 16:43:42 -0400

    wqsdax

--- src/main/java/com/misonet/interceptor/UserValidatorClass.java
+++ src/main/java/com/misonet/interceptor/UserValidatorClass.java
@@ -19,6 +19,12 @@ public class UserValidatorClass extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
 
+    	 response.setHeader("Access-Control-Allow-Origin", "*");
+    	    response.setHeader("Access-Control-Allow-Methods",
+    	            "POST, GET, OPTIONS,PUT, HEAD, DELETE");
+    	    response.setHeader("Access-Control-Max-Age", "3600");
+    	    response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    	    response.setHeader("Access-Control-Allow-Credentials", "true");
 
         if (request.getRequestURI().contains("login")) {
         	
@@ -36,6 +42,8 @@ public class UserValidatorClass extends HandlerInterceptorAdapter {
             response.sendRedirect("");
             return false;
         }
+        
+
 
         
 
